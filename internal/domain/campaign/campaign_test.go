55c0b94b1fb0966611da60b20fd5dde81e27e5fa
EduDanelli <eduardodanelli95@outlook.com>
2024-02-24 09:51:06 -0300

    utilizada biblioteca externa Testify

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -1,21 +1,33 @@
 package campaign
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewCampaign(t *testing.T) {
+	assert := assert.New(t)
 	name := "Campaign X"
 	content := "Body"
 	contacts := []string{"email@e.com", "email2@e.com"}
 
 	campaign := NewCampaign(name, content, contacts)
 
-	if campaign.ID != "1" {
-		t.Errorf("Expected 1")
-	} else if campaign.Name != name {
-		t.Errorf("Expected correct name")
-	} else if campaign.Content != content {
-		t.Errorf("Expected correct content")
-	} else if len(campaign.Contacts) != len(contacts) {
-		t.Errorf("Expected correct contacts")
-	}
+	// teste feito utilizando biblioteca externa Testify
+	assert.Equal(campaign.ID, "1")
+	assert.Equal(campaign.Name, name)
+	assert.Equal(campaign.Content, content)
+	assert.Equal(len(campaign.Contacts), len(contacts))
+
+	// teste feito na mão
+	// if campaign.ID != "1" {
+	// 	t.Errorf("Expected 1")
+	// } else if campaign.Name != name {
+	// 	t.Errorf("Expected correct name")
+	// } else if campaign.Content != content {
+	// 	t.Errorf("Expected correct content")
+	// } else if len(campaign.Contacts) != len(contacts) {
+	// 	t.Errorf("Expected correct contacts")
+	// }
 }
